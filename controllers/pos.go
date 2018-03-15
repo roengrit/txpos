e6614fedeb6a01f85e3c613e22970533ead1639b
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2018-03-15 14:21:27 +0700

    pos html complete

--- controllers/pos.go
+++ controllers/pos.go
@@ -1,5 +1,7 @@
 package controllers
 
+import "time"
+
 //POSController POSController
 type POSController struct {
 	BaseController
@@ -7,7 +9,11 @@ type POSController struct {
 
 //Get _
 func (c *POSController) Get() {
+	c.Data["CurrentDate"] = time.Now()
 	c.Layout = "layout.html"
 	c.TplName = "pos/pos.html"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["html_head"] = "pos/pos-style.html"
+	c.LayoutSections["scripts"] = "pos/pos-script.html"
 	c.Render()
 }
